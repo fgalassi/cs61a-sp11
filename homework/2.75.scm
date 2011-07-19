c2e02e8506b479ff280cdd6d7d887167407042ff
Federico Galassi <federico.galassi@gmail.com>
2011-07-19 14:28:57 +0200

    ex 2.75

---
+++ homework/2.75.scm
@@ -0,0 +1,7 @@
+(define (make-from-ang-mag r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          (else (error "Unknown operation: " op)))))
