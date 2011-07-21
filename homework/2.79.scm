57cebec17357b53975687869a04fef1346dc9ca4
Federico Galassi <federico.galassi@gmail.com>
2011-07-21 16:20:32 +0200

    fixed nomenclature in 2.79

--- homework/2.79.scm
+++ homework/2.79.scm
@@ -1,18 +1,18 @@
 
 ; scheme-number package
-(define (equ?-scheme-number x y) (= x y))
+(define (equ-scheme-number? x y) (= x y))
 ; ...
-(put 'equ? '(scheme-number scheme-number) equ?-scheme-number)
+(put 'equ? '(scheme-number scheme-number) equ-scheme-number?)
 
 ; rational package
-(define (equ?-rat x y) (and (= (numer x) (numer y)) (= (denom x) (denom y))))
+(define (equ-rat? x y) (and (= (numer x) (numer y)) (= (denom x) (denom y))))
 ; ...
-(put 'equ? '(rational rational) equ?-rat)
+(put 'equ? '(rational rational) equ-rat?)
 
 ; complex package
-(define (equ?-complex x y) (and (= (imag-part x) (imag-part y)) (= (real-part x) (real-part y))))
+(define (equ-complex? x y) (and (= (imag-part x) (imag-part y)) (= (real-part x) (real-part y))))
 ; ...
-(put 'equ? '(complex complex) equ?-complex)
+(put 'equ? '(complex complex) equ-complex?)
 
 ; make global
 (define (equ? x y) (apply-generic 'equ? x y))
