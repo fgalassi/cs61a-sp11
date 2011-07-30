8fe2b52f64de783187fcf79cec8d325afef0dab0
Federico Galassi <federico.galassi@gmail.com>
2011-07-30 10:14:01 +0200

    homework 7 ex. extra

---
+++ homework/h7.extra.scm
@@ -0,0 +1,13 @@
+;              [door]
+;               open
+;             /      \ 
+;            /        \
+;   [glass door]    [electric door]
+;       clean             open
+;           \          /
+;            \        /
+;      [electric glass door]
+; 
+; if i try to open the electric glass door, i wanna open it the electric door
+; way. not the glass door way, which in turn is the normal door way.
+;
