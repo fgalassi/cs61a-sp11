8d07e582d9edaa194d4c11ca0f19cc7b31ac503a
Federico Galassi <federico.galassi@gmail.com>
2011-07-30 14:01:38 +0200

    ex. 3.10

---
+++ homework/3.10.scm
@@ -0,0 +1,8 @@
+; for each instance
+; w1 and w2
+; in the "param version" there's a lambda doing the work and an environment binding balance
+; in the "let version" there's 2 lambdas and 2 environments
+; the first lambda is the "let lambda" and the first environment binds initial-value
+; the second lambda does the real work and the second environment binds balance
+;
+; 
