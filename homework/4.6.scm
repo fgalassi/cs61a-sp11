471127cfba5c60b9e7196d1125f3ef778e1fc099
Federico Galassi <federico.galassi@gmail.com>
2011-10-01 17:31:07 +0200

    homework 4.6

---
+++ homework/4.6.scm
@@ -0,0 +1,19 @@
+(load "4.3")
+
+(define (let-bindings exp) (cadr exp))
+(define (let-body exp) (cddr exp))
+
+(define (binding-variable binding) (car binding))
+(define (binding-exp binding) (cadr binding))
+
+(define (let->combination exp)
+  (cons
+    (make-lambda
+      (map binding-variable (let-bindings exp))
+      (let-body exp))
+    (map binding-exp (let-bindings exp))))
+
+(define (eval-let exp env)
+  (mc-eval (let->combination exp) env))
+
+(mc-put 'let eval-let)
