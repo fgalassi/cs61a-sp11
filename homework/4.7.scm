a28cbb31853e38c5ef56ec1b1a96f03db522c59a
Federico Galassi <federico.galassi@gmail.com>
2011-10-01 20:00:15 +0200

    homework 4.7

---
+++ homework/4.7.scm
@@ -0,0 +1,20 @@
+(load "4.6")
+
+(define (make-let bindings body)
+  (list 'let bindings body))
+
+(define (let*->nested-lets exp)
+  (define (expand-let* bindings body)
+    (if (null? bindings)
+      body
+      (make-let
+        (list (car bindings))
+        (expand-let* (cdr bindings) body))))
+  (expand-let* (let-bindings exp) (let-body exp)))
+
+(define (eval-let* exp env)
+  (mc-eval (let*->nested-lets exp) env))
+
+(mc-put 'let* eval-let*)
+
+(let* ((x 3) (y (+ x 2)) (z (+ x y 5))) (* x z))
